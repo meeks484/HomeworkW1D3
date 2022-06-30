91a4788acad58d04b27fbfd6e35ab90f3e97c347
meeks484 <meeks484@gmail.com>
2022-06-29 23:59:21 -0600

    Initial Commit of CreditCard Class

---
+++ src/CreditCard.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+public class CreditCard {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter a credit card number as a long integer");
+        long number = Long.parseLong(myObj.nextLine());
+        if (isValid(number)){System.out.println(number+" is valid");}
+        else{System.out.println(number+" is invalid");}
+    }
+
+    public static boolean isValid(long number){
+        if (getSize(number)>=13 && getSize(number)<=16){
+            if (prefixMatched(number,4) || prefixMatched(number,5) || prefixMatched(number,37) || prefixMatched(number,6)){
+                if ((sumOfDoubleEvenPlace(number)+sumOfOddPlace(number))%10==0){return true;}
+                else{return false;}
+            }
+            else{return false;}
+        }
+        else{return false;}
+
+    }
+
+    public static int sumOfDoubleEvenPlace(long number){
+        boolean secondDigit = false;
+        int sumSingleDigits = 0;
+        while (number>0){
+            if (secondDigit){
+                sumSingleDigits += getDigit(((int)(number%10L))*2);
+                secondDigit = false;
+            }
+            else{secondDigit = true;}
+            number = number / 10L;
+
+        }
+        return sumSingleDigits;
+    }
+
+    public static int getDigit(int number){
+        if (number>=10){return number/10+number%10;}
+        else{return number;}
+    }
+
+    public static int sumOfOddPlace(long number){
+        boolean oddDigit = true;
+        int sumOddDigit = 0;
+        while (number>0){
+            if (oddDigit){
+                sumOddDigit += (int)(number%10L);
+                oddDigit = false;
+            }
+            else{oddDigit = true;}
+            number = number / 10L;
+        }
+        return sumOddDigit;
+    }
+
+    public static boolean prefixMatched(long number, int d){
+        if (getPrefix(number,getSize((long)d))==d){return true;}
+        else{return false;}
+    }
+
+    public static int getSize(long d){
+        int numberOfDigits =0;
+        while (d>0){
+            numberOfDigits++;
+            d = d / 10L;
+        }
+        return numberOfDigits;
+    }
+
+    public static long getPrefix(long number, int k){
+        int totalDigits = getSize(number);
+        if (totalDigits>k){
+            return (long) (number/(Math.pow(10,(totalDigits-k))));//chop off the right side digits we don't want
+        }
+        else{return number;}
+    }
+}
